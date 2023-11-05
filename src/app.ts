c86a9e4847f325e2b5ac7aaa7ddd269ee5ac6724
Hussenet Thomas <Lejamon.c@protonmail.com>
2023-11-05 11:55:59 +0100

    m

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,18 @@
 import express, { Express } from "express";
+import cors from "cors"; // Import the CORS package
 
 const app: Express = express();
 
+// Setup CORS
+const corsOptions = {
+  origin: 'https://near.org', // This should be the domain you want to allow connections from
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
+app.use(cors(corsOptions)); // Use the CORS middleware with the specified options
+
 import checkBTCBalance from "./routes/checkBTCBalance";
 import sign from "./routes/sign";
-//tb1qd5f0574r3mghfglmvlpfmknquarjw20r9mwm4v
 
 app.use("/api/check-btc-balance", checkBTCBalance);
 app.use("/api/sign", sign);
